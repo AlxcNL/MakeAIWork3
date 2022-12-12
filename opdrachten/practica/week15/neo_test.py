669487c851d41bc89afd27ac9c44aaf5ac5b7295
AlxcNL <J.A.Boogaard@hr.nl>
2022-12-12 11:01:51 +0100

    Added docker script neo4j

---
+++ opdrachten/practica/week15/neo_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from py2neo import Node
+
+nicole = Node("Person", name="Nicole", age=24)
+drew = Node("Person", name="Drew", age=20)
+
+mtdew = Node("Drink", name="Mountain Dew", calories=9000)
+cokezero = Node("Drink", name="Coke Zero", calories=0)
+
+coke = Node("Manufacturer", name="Coca Cola")
+pepsi = Node("Manufacturer", name="Pepsi")
+
+graph.create(nicole | drew | mtdew | cokezero | coke | pepsi)
