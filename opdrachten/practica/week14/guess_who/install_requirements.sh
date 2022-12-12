45f71fcc264925242d59001efd9b6cf6c58b15d6
AlxcNL <J.A.Boogaard@hr.nl>
2022-12-12 09:02:51 +0100

    Hardening libraries

--- opdrachten/practica/week14/guess_who/install_requirements.sh
+++ opdrachten/practica/week14/guess_who/install_requirements.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-python -m pip install --no-cache-dir -r requirements.txt
+  if (! command -v "conda" &> /dev/null ) then
+    echo "Install with pip\n"
+    python -m pip install --upgrade pip
+    python -m pip install --no-cache-dir -r requirements.txt
+
+  else
+    conda update -n base -c conda-forge conda
+    conda install --yes -c conda-forge \
+      opencv \
+      pathlib \
+      pymongo \
+      pydata \
+      pyspark \
+      scrapy
+
+  fi
+
+
