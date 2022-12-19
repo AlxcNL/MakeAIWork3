1aae97e3089d588cd6f9c5d0318506bce437a32c
AlxcNL <J.A.Boogaard@hr.nl>
2022-12-19 22:50:21 +0100

    Added guess_who version 3

---
+++ opdrachten/practica/week15/guess_who/3/sh/start_mongo.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function makeWindowsProof {
+    hostdirData=$(cygpath -w -p ${hostdirData})
+    prefix="winpty "
+}
+
+# Windows environment?
+prefix=""
+unameOut="$(uname -s)"
+os="${unameOut:0:7}"
+case "${os}" in
+    Linux*)     machine="Linux";;
+    Darwin*)    machine="Mac";;
+    CYGWIN*)    machine="Cygwin" && makeWindowsProof;;
+    MINGW*)     machine="Git Bash" && makeWindowsProof;;
+    *)          machine="UNKNOWN:${os}"
+esac
+
+# Container
+
+containerName="python-ai-mongo"
+
+composePath="docker/compose"
+composefile="${composePath}/python-ai-mongo.yaml"
+
+cmd="${prefix}docker/compose/up.sh ${composefile}"
+
+printf "%s cmd : \n\t%s\n\n" "$0" "${cmd}"
+eval ${cmd}
