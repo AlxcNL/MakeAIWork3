cda6e222498ec203a18e4930f59927b106a2c08f
AlxcNL <J.A.Boogaard@hr.nl>
2023-05-23 07:46:03 +0200

    Last update

--- opdrachten/practica/week13/numpy/image_filter.py
+++ opdrachten/practica/week13/numpy/image_filter.py
@@ -2,37 +2,24 @@ import io
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
+import skimage.measure as sm
+
 
 class ImageFilter(object):
-    
-    def __init__(self, kernel):
-        self.imgKernel = kernel
-    
+    def __init__(self, kernel=None):
+        if kernel is not None:
+            self.imgKernel = kernel
+
     def convolve(self, imgTensor):
-        # imgTensorRGB = self.convert2RGB(imgTensor.copy())    
-        imgTensorRGB = imgTensor.copy() 
+        imgTensorRGB = imgTensor.copy()
         outputImgRGB = np.empty_like(imgTensorRGB)
 
         for dim in range(imgTensorRGB.shape[-1]):  # loop over rgb channels
-            outputImgRGB[:, :, dim] = sp.signal.convolve2d (
+            outputImgRGB[:, :, dim] = sp.signal.convolve2d(
                 imgTensorRGB[:, :, dim], self.imgKernel, mode="same", boundary="symm"
             )
 
         return outputImgRGB
 
-    def convert2RGB(self, imgTensor, fillValue=1):
-        """Add an alpha channel to an RGB array"""
-        # arr = plt.imread(imgFile).astype(float)
-            
-        if imgTensor.shape[-1] >= 4:
-            return imgTensor
-        
-        imgTensorFilled = np.full (
-            shape=(*imgTensor.shape[:-1], 4),
-            fill_value=fillValue,
-            dtype=imgTensor.dtype
-        )
-        
-        imgTensorFilled[:, :, :-1] = imgTensor
-
-        return imgTensorFilled
+    def downSample(self, img, blockSize=(2, 2, 1)):
+        return sm.block_reduce(image=img, block_size=blockSize, func=np.mean)
